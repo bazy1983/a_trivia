864c4df20425a21db470d760faafbbe855a393db
Mohammed Hameed <mohammed.hameed@email.com>
2018-02-25 17:55:47 -0600

    built countdown functionality

--- js/logic.js
+++ js/logic.js
@@ -1,7 +1,27 @@
 $(document).ready(function (){
-
     
+    var time = 11,
+        arrIndex = 0;
 
+    var timeFunction = function () {
+        var countdown =  setInterval (function(){
+        time --;
+        $(".timer").html(time) // pass the value to document
+        console.log (time);
+        if (time === 0){
 
+            clearInterval(countdown);
+            setTimeout ( function() {
+            resetTimer();
+            }, 2000);
 
+        } 
+    }, 1000);}
+
+    function resetTimer (){
+        time = 11
+        timeFunction();
+    }
+
+    timeFunction();
 });

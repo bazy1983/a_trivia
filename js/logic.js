8d31a52da37f6eac2e2c1ff42efb0c22d41029f7
Mohammed Hameed <mohammed.hameed@email.com>
2018-02-25 21:06:19 -0600

    added append functionality to display all the questions and the answers to the document

--- js/logic.js
+++ js/logic.js
@@ -3,25 +3,43 @@ $(document).ready(function (){
     var time = 11,
         arrIndex = 0;
 
+    function passQuestion (i) {
+        $(".question").html(myQuestions[i].question);
+
+        for (var ai = 0; ai < myQuestions[i].answers.length; ai++){
+            var myAnswers = $("<p>" + myQuestions[i].answers[ai] + "</p>");
+            $(".choices").append(myAnswers);
+        }
+      
+    };
+    passQuestion(arrIndex);
+
     var timeFunction = function () {
         var countdown =  setInterval (function(){
         time --;
         $(".timer").html(time) // pass the value to document
-        console.log (time);
-        if (time === 0){
+            if (time === 0){ //when time is up
 
-            clearInterval(countdown);
-            setTimeout ( function() {
-            resetTimer();
-            }, 2000);
+                clearInterval(countdown);
+                setTimeout ( function() {
+                resetTimer();
+                }, 2000);
 
-        } 
-    }, 1000);}
+            } 
+        }, 1000);
+    }
 
     function resetTimer (){
-        time = 11
-        timeFunction();
-    }
+        arrIndex++
+        if (arrIndex < myQuestions.length) {
+            $(".choices").empty();
+            passQuestion(arrIndex);
+            time = 11
+            timeFunction();
+        } else { // when run out of questions
+            $(".question").html("no more questions");
+        }
+    };
 
     timeFunction();
 });
